80fdaa2814b48e9a8f9e7b3ad6e6dc8e34c3c7fc
Maula Faiz A <maulafaiz97@gmail.com>
2024-05-15 07:46:56 +0700

    escape ;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <p>Hi, I'm Maula</p>
-      <p>Software engineer with 4+ years experience. I've worked with various tech stack and language from python, go, and mainly java.</p>
+      <p>Hi, I&apos;m Maula</p>
+      <p>Software engineer with 4+ years experience. I&apos;ve worked with various tech stack and language from python, go, and mainly java.</p>
     </main>
   );
 }

746b73b7f7581507bfe5e12c963e6242b84d8b7b
Maula Faiz A <maulafaiz97@gmail.com>
2024-05-17 07:55:07 +0700

    add github & x link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <p>Hi, I&apos;m Maula</p>
-      <p>Software engineer with 4+ years experience. I&apos;ve worked with various tech stack and language from python, go, and mainly java.</p>
+    <main className="flex flex-col text-center p-24">
+      <div>
+        <p>Hi, I&apos;m Maula</p>
+        <br></br>
+        <p className="text-justify lg:text-center">Software engineer with 4+ years experience. I&apos;ve worked with various tech stack and language from python, go, and mainly java.</p>
+      </div>
+      <div className="mt-6">
+        <p>Let&apos;s get in touch!</p>
+        <div className="flex flex-row justify-center gap-4">
+          <a href="https://x.com/maulafaizz" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon className="fa-3x" icon={ faXTwitter } />
+          </a>
+          <a href="https://github.com/maulafaizz" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon className="fa-3x" icon={ faGithub } />
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
